e6dfe01ad06551e4fa251001b34f02abcba2cfa1
aqur1n <60589309+aqur1n@users.noreply.github.com>
2023-03-09 21:53:57 +0700

    update: Optimization of space and speed in explorer
    * Removed a meaningless function with checks on the gpu.set
    * Limiting the length of the path on the screen
    * Removed temporary comments

--- run/2.lua
+++ run/2.lua
@@ -8,18 +8,9 @@ local function getkeys(tbl)
     return keys
 end
 
-local function gpuset(x, y, str)
-    if y < 5 or y > sh - 3 then return end
-    gpuproxy.set(x, y, str)
-end
-
 local function remove(str) 
     local tb = {}
-    for i in string.gmatch(str, "([^\\/]+)") do 
-        if i ~= "" then
-            table.insert(tb, i .. "/") 
-        end
-    end
+    for i in string.gmatch(str, "([^\\/]+)") do table.insert(tb, i .. "/") end
     table.remove(tb)
     return tb
 end
@@ -33,39 +24,40 @@ function disks.draw()
     normalcolour()
     gpuproxy.fill(2, 3, sw - 2, 1, " ")
     gpuproxy.fill(1, 5, sw, sh - 7, " ")
-    if fs then gpuproxy.set(2, 3, pth)
+    if fs then gpuproxy.set(2, 3, string.sub(pth, 1, sw - 2))
     else gpuproxy.set(2, 3, "Disk and File Manager") end
 
     for i = 1, #lst do
-        if i >= sh - 7 + page then break
-        elseif i == indx then inversioncolour() 
-        else normalcolour() 
+        if i >= page then 
+            if i >= sh - 7 + page then break
+            elseif i == indx then inversioncolour() 
+            else normalcolour() 
+            end
+            gpuproxy.set(2, 5 + i - page, lst[i])
         end
-        gpuset(1, 5 + i - page, lst[i])
     end
 end
 
 function disks.signal(signal)
     if signal[1] == "key_down" then
-        if signal[4] == 208 then -- down
+        if signal[4] == 208 then
             if indx < #lst then 
                 indx = indx + 1 
                 if indx >= sh - 7 then 
                     page = page + 1
                 end
             end
-        elseif signal[4] == 200 then  -- up
+        elseif signal[4] == 200 then
             if indx > 1 then 
                 indx = indx - 1 
                 if indx - page <= 0 and page > 0 then 
                     page = page - 1
                 end
             end
-        elseif signal[4] == 28 then -- enter
+        elseif signal[4] == 28 then
             if fs then
                 if lst[indx] == "..." then
                     if pth == "/" then 
-                        -- disks.init calling disks.draw
                         fs, pth, indx, lst, page = nil, "/", 1, getkeys(component.list("filesystem")), 0
                     else 
                         pth = "/" .. table.concat(remove(pth))
@@ -90,10 +82,6 @@ function disks.signal(signal)
             end
         end
         disks.draw() 
-    -- elseif signal[1] == "component_added" then
-    --     if signal[3] == "filesystem" then
-    --         disks.draw() 
-    --     end
     elseif signal[1] == "component_removed" then
         if signal[3] == "filesystem" and signal[2] == fs.address then
             disks.init()
